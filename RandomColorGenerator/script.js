36f2c40afacc5a1d3b5a2f6ab44711c68f10e163
Abhishek <goswamiabhi9729@gmail.com>
2024-12-12 18:26:09 +0530

    comments added

--- RandomColorGenerator/script.js
+++ RandomColorGenerator/script.js
@@ -1,5 +1,4 @@
 // generate a random background color
-
 const randomColor = function (){
     const hex = '0123456789ABCDEF';
     let color = '#';
@@ -11,12 +10,15 @@ const randomColor = function (){
 
 let interval;
 
+// change background color every second
 const bgChange = function() {
     interval = setInterval(() => {
         document.body.style.backgroundColor = randomColor()
     }, 1000);
 }
 
+
+// stop changing background color
 const stopChange = function() {
     clearInterval(interval);
     interval = null;
